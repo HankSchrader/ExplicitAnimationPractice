d512469e8ef1e1de676371b8e6de4707761a9280
Erik Mikac <mikacew@gmail.com>
2021-03-07 08:18:26 -0500

    Initial Commit

---
+++ Shared/ExplicitAnimationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplicitAnimationPracticeApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ExplicitAnimationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
